f81bf3c594301183ade2ef71e1e6ac901f560de9
JuKu <kuenzel.justin@t-online.de>
2021-04-19 18:30:33 +0200

    Initial commit

---
+++ src/main/java/com/jukusoft/browsergame/BrowsergameRtsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jukusoft.browsergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowsergameRtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowsergameRtsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jukusoft/browsergame/BrowsergameRtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jukusoft.browsergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowsergameRtsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
