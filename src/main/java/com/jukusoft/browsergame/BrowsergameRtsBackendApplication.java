e3687855dd3951116df4b1135472640db6052ed6
JuKu <kuenzel.justin@t-online.de>
2021-04-20 16:39:41 +0200

    Add new BuildingEntity

--- src/main/java/com/jukusoft/browsergame/BrowsergameRtsBackendApplication.java
+++ src/main/java/com/jukusoft/browsergame/BrowsergameRtsBackendApplication.java
@@ -2,8 +2,16 @@ package com.jukusoft.browsergame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.jukusoft.browsergame", "com.jukusoft.authentification.jwt"})
+@EnableCaching
+@EnableScheduling
+@EntityScan({"com.jukusoft.browsergame", "com.jukusoft.authentification.jwt"})
+@EnableJpaRepositories({"com.jukusoft.browsergame", "com.jukusoft.authentification.jwt"})
 public class BrowsergameRtsBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/jukusoft/browsergame/entity/building/BuildingEntity.java
@@ -0,0 +1,39 @@
+package com.jukusoft.browsergame.entity.building;
+
+import com.jukusoft.browsergame.entity.general.AbstractEntity;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "buildings", indexes = {
+        //@Index(columnList = "email", name = "email_idx"),
+}, uniqueConstraints = {
+        //@UniqueConstraint(columnNames = "username", name = "username_uqn")
+})
+@Cacheable//use second level cache
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class BuildingEntity extends AbstractEntity {
+
+    @Size(min = 2, max = 900)
+    @Column(name = "name", unique = true, nullable = false, updatable = true)
+    @NotEmpty(message = "value is required")
+    private String uniqueName;
+
+    @Size(min = 2, max = 255)
+    @Column(name = "title", unique = false, nullable = false, updatable = true)
+    @NotEmpty(message = "title is required")
+    private String title;
+
+    @Column(name = "min_level", unique = false, nullable = false, updatable = true)
+    private int minLevel;
+
+    @Column(name = "max_level", unique = false, nullable = false, updatable = true)
+    private int maxLevel;
+
+}
