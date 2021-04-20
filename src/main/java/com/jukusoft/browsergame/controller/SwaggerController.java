2268e0a81e48fe6ad0848778dbb9ad0733e83674
JuKu <kuenzel.justin@t-online.de>
2021-04-20 16:57:34 +0200

    Add swagger configuration

---
+++ src/main/java/com/jukusoft/browsergame/controller/SwaggerController.java
@@ -0,0 +1,24 @@
+package com.jukusoft.browsergame.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for swagger documentation.
+ *
+ * @author Vincent Gerber
+ */
+@Controller
+public class SwaggerController {
+
+    /**
+     * Redirect all requests for /swagger to the html page.
+     *
+     * @return Redirect String
+     */
+    @GetMapping("/swagger")
+    public String redirectToSwaggerUI() {
+        return "redirect:/swagger-ui.html";
+    }
+
+}
